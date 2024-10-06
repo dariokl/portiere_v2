33550b55835100eca75a80f9e501561f4c94f453
dariok <darioklopic@gmail.com>
2024-10-06 22:58:05 +0200

    budget widget WIP

---
+++ src/components/dashboard/overview/BudgetModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <VaCard>
+    <VaCardTitle class="rounded-md bg-indigo-600">
+      <div class="flex justify-between w-full">
+        <div class="flex flex-col w-full">
+          <HoverableIcon icon-name="shopping_bag" cursor="default" icon-color="#ffff" />
+          <span class="block mt-2 text-white">Montly Budget</span>
+        </div>
+      </div>
+    </VaCardTitle>
+    <VaCardContent> 
+            
+
+    </VaCardContent>
+  </VaCard>
+</template>
+
+<script setup>
+import HoverableIcon from '@/components/HoverableIcon.vue'
+</script>
